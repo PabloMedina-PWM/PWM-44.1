e1d0e9583a194b231ecf57ccab4f540eaf77c4d2
kevinjfa <kevin.fa7@outlook.com>
2025-03-14 12:04:41 +0000

    Fixed minor issues on content-viewer and header responsive styles.

--- code/js/content-viewer.js
+++ code/js/content-viewer.js
@@ -1,40 +1,10 @@
 async function changeContent(page, json) {
-
     document.getElementById("titulo1").innerText = json.titulo;
-
-    for (let i = 0; i < json.nFilas; i++) {
-        let jsonfila = "fila" + (i + 1);
-        let fila = json[jsonfila];
-        let tr = addRow(fila["col1"], fila["col2"], fila["col3"], fila["col4"], json.textoBotonTabla);
-        document.getElementById("table-viewer").getElementsByTagName("tbody")[0].appendChild(tr);
-    }
-
-    json.columnasAOcultar.forEach(columna => {
-        document.querySelectorAll(columna).forEach(e => e.style.display = "none");
-    });
-    for (let i=0; i<=json.columnas.length; i++) {
-        let nombre = "heading" + (i+1);
-        document.getElementById(nombre).innerText = json.nombreColumnas[i];
-    }
-    for (let i = 1; i < json.filtros+1; i++) {
-        let filtro = "filtro" + i;
-        document.getElementById(filtro).options[0].text = json.nombreFiltros[i-1];
-    }
-    for (let i = 3; i > json.filtros; i--) {
-        let filtro = "filtro" + i;
-        document.getElementById(filtro).style.display = "none";
-    }
-    document.querySelectorAll(".button_colum").forEach(e => {
-        e.textContent = json.textoBotonTabla;
-        e.onclick = function () {
-            window.location.href = json.enlaceBotonTabla;
-        }
-    });
-
-    document.getElementById("add-button").innerText = json.textoBotonAñadir;
-    document.getElementById("add-button").onclick = function () {
-        window.location.href = json.enlaceBotonAñadir;
-    }
+    addTable(json);
+    hideShowColumns(json);
+    hideShowFilters(json);
+    configTableButtons(json);
+    configAddButton(json);
 }
 
 function addRow(col1, col2, col3, col4, col5) {
@@ -59,4 +29,52 @@ function addRow(col1, col2, col3, col4, col5) {
 
 
     return tr;
+}
+
+
+function addTable(json) {
+    for (let i = 0; i < json.nFilas; i++) {
+        let jsonfila = "fila" + (i + 1);
+        let fila = json[jsonfila];
+        let tr = addRow(fila["col1"], fila["col2"], fila["col3"], fila["col4"], json.textoBotonTabla);
+        document.getElementById("table-viewer").getElementsByTagName("tbody")[0].appendChild(tr);
+    }
+}
+
+function hideShowColumns(json) {
+    json.columnasAOcultar.forEach(columna => {
+        document.querySelectorAll(columna).forEach(e => e.style.display = "none");
+    });
+    for (let i=0; i<=json.columnas.length; i++) {
+        let nombre = "heading" + (i+1);
+        document.getElementById(nombre).innerText = json.nombreColumnas[i];
+    }
+}
+
+function hideShowFilters(json) {
+    for (let i = 1; i < json.filtros+1; i++) {
+        let filtro = "filtro" + i;
+        document.getElementById(filtro).options[0].text = json.nombreFiltros[i-1];
+    }
+    for (let i = 3; i > json.filtros; i--) {
+        let filtro = "filtro" + i;
+        document.getElementById(filtro).style.display = "none";
+    }
+}
+
+
+function configTableButtons(json) {
+    document.querySelectorAll(".button_colum").forEach(e => {
+        e.textContent = json.textoBotonTabla;
+        e.onclick = function () {
+            window.location.href = json.enlaceBotonTabla;
+        }
+    });
+}
+
+function configAddButton(json) {
+    document.getElementById("add-button").innerText = json.textoBotonAñadir;
+    document.getElementById("add-button").onclick = function () {
+        window.location.href = json.enlaceBotonAñadir;
+    }
 }
