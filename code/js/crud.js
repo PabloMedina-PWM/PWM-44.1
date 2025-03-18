4bbb40db04ccfae37a6c4b2e36f97911f7b20de6
Medichi2002 <pablo.medina108@alu.ulpgc.es>
2025-03-18 17:35:41 +0000

    Refactor

--- code/js/crud.js
+++ code/js/crud.js
@@ -1,12 +1,22 @@
 
-function setChanges(type){
+function changeContent(type){
     switch (type) {
-        case 'artista':
+        case 'crudArtista':
             document.querySelector('.crudTitle').textContent = "Artista";
+            document.getElementById('surnames').textContent = "Apellidos";
+            document.getElementById('address').placeholder = "Apellidos";
+            document.querySelector('.c-n-task').style.display = "none";
+            document.querySelector('.c-password').style.display = "none";
+            document.querySelector('.c-province').style.display = "none";
             break;
-    }
-}
 
-function changer(type) {
-    setChanges(type);
+        case 'crudEmpleado':
+            document.querySelector('.crudTitle').textContent = "Empleado";
+            document.getElementById('surnames').textContent = "Apellidos";
+            document.getElementById('address').placeholder = "Apellidos";
+            document.querySelector('.c-n-task').style.display = "none";
+            document.querySelector('.c-province').style.display = "none";
+            break;
+
+    }
 }
