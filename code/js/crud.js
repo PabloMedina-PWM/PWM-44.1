2af6c407d45711d0a4dc0ea44caf3f9b71cd831a
Medichi2002 <pablo.medina108@alu.ulpgc.es>
2025-03-25 17:16:20 +0000

    Crud.js and json refactor

--- code/js/crud.js
+++ code/js/crud.js
@@ -1,22 +1,34 @@
 
-function changeContent(type){
-    switch (type) {
-        case 'crudArtista':
-            document.querySelector('.crudTitle').textContent = "Artista";
-            document.getElementById('surnames').textContent = "Apellidos";
-            document.getElementById('address').placeholder = "Apellidos";
-            document.querySelector('.c-n-task').style.display = "none";
-            document.querySelector('.c-password').style.display = "none";
-            document.querySelector('.c-province').style.display = "none";
-            break;
+async function changeContent(page, json) {
 
-        case 'crudEmpleado':
-            document.querySelector('.crudTitle').textContent = "Empleado";
-            document.getElementById('surnames').textContent = "Apellidos";
-            document.getElementById('address').placeholder = "Apellidos";
-            document.querySelector('.c-n-task').style.display = "none";
-            document.querySelector('.c-province').style.display = "none";
-            break;
+    document.querySelector('.crudTitle').textContent = json.titulo;
 
+    changeTextContent(json);
+    changePlaceHolders(json);
+    hideRows(json);
+
+}
+
+function changeTextContent(json){
+    if (json.cambiar[0] === 0) return;
+    for (let i = 1; i <= json.cambiar[0]; i+=2){
+        document.getElementById(json.cambiar[i]).textContent = json.cambiar[i+1];
     }
 }
+
+function changePlaceHolders(json){
+    if (json.placeholders[0] === 0) return;
+    for (let i = 1; i <= json.placeholders[0]; i+=2){
+        document.getElementById(json.placeholders[i]).placeholder = json.placeholders[i+1];
+    }
+}
+
+
+function hideRows(json){
+    if (json.ocultar[0] === 0) return;
+    for (let i = 1; i <= json.ocultar[0]; i++){
+        document.querySelector(json.ocultar[i]).style.display = "none";
+    }
+}
+
+
