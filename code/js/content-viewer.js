e78bf86331973b4feb39c203e275c2db1a0726be
kevinjfa <kevin.fa7@outlook.com>
2025-03-27 09:05:49 +0000

    Added task id and email to every task

--- code/js/content-viewer.js
+++ code/js/content-viewer.js
@@ -163,7 +163,7 @@ async function addData(json) {
 }
 
 async function addOptionToFilter(filterName, optionName) {
-    if (filterName === "fecha" || filterName === "capacidad") return;
+    if (filterName === "fecha" || filterName === "capacidad" || filterName === "emailEmpleado" || filterName === "idTarea") return;
     if (filterName === "direccion") return;
     let filter = document.getElementById(filterName);
     if (!filter) {
